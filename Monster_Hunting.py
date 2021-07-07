880167e3b3daea617697cef71b3651e82ed8a528
pavan <41847067+pavanrrathoriya@users.noreply.github.com>
2021-07-07 17:25:42 +0530

    Update Monster_Hunting.py

--- Monster_Hunting.py
+++ Monster_Hunting.py
@@ -1,1 +1,23 @@
+n=int(input( ))
+e=int(input())
+p=[]
+b=list()
+print()
+for i in range(n):
+    p.append(int(input()))
+
+
+for i in range(n):
+    b.append(int(input()))
+pb=dict(zip(p, b))
+p.sort()
+c=0
+for i in range(n):
+    if e>=p[i]:
+        e+=pb[p[i]]
+        c+=1
+    else:
+        return c
+return c
+
 
